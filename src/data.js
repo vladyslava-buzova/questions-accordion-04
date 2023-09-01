500f0a30216ee046a9de0266a1add8e67d5dede4
Vladyslava Buzova <dev.vladyslava@gmail.com>
2023-09-01 16:02:07 -0600

    final commit

--- src/data.js
+++ src/data.js
@@ -1,28 +1,28 @@
 const questions = [
   {
     id: 1,
-    title: 'Do I have to allow the use of cookies?',
-    info: 'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+    title: 'How do I search for a suitable hotel or apartment for my stay?',
+    info: 'It is very easy. At first type (enter) name of your destination/hotel including date of your stay into the blue "Search box". Than just click on red button "Search". Immediately, all the available rooms will show up including prices for whole stay (number of nights you filled previously). ',
   },
   {
     id: 2,
-    title: 'How do I change my My Page password?',
-    info: 'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+    title: 'How do I get to know which facilities / services each hotel offers?',
+    info: 'Please, have a look at the section Hotel basic information. In this section you will also find the check in/out information. Hotel facilities / services are stated in Hotel basic information, just below the the offer of available rooms. In this section you will also find the check in/out information. Please note, that some of the facilities might be off-site and also for extra charge.',
   },
   {
     id: 3,
-    title: 'What is BankID?',
-    info: 'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+    title: 'Is it possible to order an extra bed/baby cot and how much will it be?',
+    info: 'The extra bed/baby cots hotel policy is written in "Payment and cancellation conditions" in section "Children and Extra Bed Policy". Each accommodation has its own extra bed policy, therefore its necessary to always check it in order to find out if the extra bed will be possible or not. ',
   },
   {
     id: 4,
-    title: 'Whose birth number can I use?',
-    info: 'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+    title: 'How do I book accommodation?',
+    info: 'Booking is possible only by completing the reservation form on our web site. Please, select the dates of your stay and type of rooms you have chosen. The system will show you the price for the accommodation (always check the room conditions).',
   },
   {
     id: 5,
-    title: 'When do I recieve a password ordered by letter?',
-    info: 'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+    title: 'How do I know that my reservation is confirmed?',
+    info: 'After completing and sending us your order form, you will get an automatic receipt e-mail containing your order number. Within 30 minutes, you will receive Accommodation confirmation.',
   },
 ];
 export default questions;
